d3f5fd9e94e4010bb3a43330cf9f0241c86b93b4
Adr3ynj <ayanezzn1@est.ups.edu.ec>
2024-01-29 13:16:19 -0500

    Add(): Add printtriangle Function

--- Poo/src/Main.java
+++ Poo/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printtriangle(5, 5);
     }
 }
+
+    public static void printtriangle(int length, int max) {
+        for (int i = 1; i <= (length); i++) {
+            System.out.print(" * ");
+            for (int j = i + 1; j <= max; j++) {
+                System.out.print(" * ");
+            }
+            System.out.print("\n");
+        }
+    }
